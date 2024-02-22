fb6c20e94762f5508c5cc14339be85a386703527
Miguel Guatava <mf.guatava47@uniandes.edu.co>
2024-02-22 15:32:35 -0500

    Se implementa servicios

---
+++ servicios/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import redis
+import random
+import json
+
+app = Flask(__name__)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
+
+@app.route('/servicios', methods=['GET'])
+def health_check():
+    if random.randint(1, 100) <= 10:
+        status = 'down'
+    else:
+        status = 'up'
+    
+    message = json.dumps({'service': 'servicios', 'status': status})
+    redis_client.publish('health_checks', message)
+    return "Health status updated"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003)
